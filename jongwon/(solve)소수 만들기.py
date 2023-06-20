cd942d8158ee5c2c0d96bcd1ef66cc4d58edb6f6
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-06-20 20:14:17 +0900

    6/20

--- jongwon/(solve)소수 만들기.py
+++ jongwon/(solve)소수 만들기.py
@@ -13,4 +13,25 @@ def solution(nums):
                 if check(nums[i], nums[j], nums[k]): # 뽑은 숫자들의 합을 소수 판별해서 true 이면 result 1증가
                     result += 1
     return result
+
+
+from itertools import combinations
+import math
+
+def is_prime_number(x):
+    #2부터 x의 제곱근까지의 모든 수를 확인하며
+    for i in range(2, int(math.sqrt(x))+1):
+        #x가 해당 수로 나누어 떨어진다면
+        if x % i == 0:
+            return False # 소수가 아님
+    return True # 소수임
+
+def solution(nums):
+    answer = 0
+    for x in combinations(nums, 3):
+        if is_prime_number(sum(x)):
+            answer += 1
+
+    return answer
+
     
