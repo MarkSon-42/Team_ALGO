81f26c4344f39cc074f1a0e12ddf846ca3868572
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-08-01 19:56:22 +0900

    8/1

---
+++ jongwon/42626. 더 맵게/(solve)더 맵게.py
@@ -0,0 +1,23 @@
+# heapq.heapify(scoville) # scoville을 힙으로 지정
+# heapq.heappop(scoville) # scoville에서 가장 작은 원소를 pop
+# heapq.heappush(scoville, new) # scoville에 섞은 스코빌 지수를 추가
+
+import heapq
+
+def solution(scoville, K):
+    heapq.heapify(scoville) # scoville을 힙으로 지정
+    mix = 0 # 섞은 횟수
+    
+    while scoville[0] <= K:
+        if len(scoville) == 1:
+            return -1
+        if scoville[0] >= K:
+            return mix
+            
+        a = heapq.heappop(scoville) # scoville에서 가장 작은 원소를 pop
+        b = heapq.heappop(scoville)
+        new = a + (b*2)
+        heapq.heappush(scoville, new) # scoville에 섞은 스코빌 지수를 추가
+        mix += 1 # 섞은 횟수 + 1
+        
+    return mix
