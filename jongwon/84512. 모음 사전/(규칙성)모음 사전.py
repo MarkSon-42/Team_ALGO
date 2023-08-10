1d8439fa9ad5c81386d79c404490a07c96cc6d71
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-08-10 11:07:16 +0900

    8/10

--- jongwon/84512. 모음 사전/(규칙성)모음 사전.py
+++ jongwon/84512. 모음 사전/(규칙성)모음 사전.py
@@ -1,23 +1,15 @@
-# 참고 : https://velog.io/@sugenius77/Python%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EB%AA%A8%EC%9D%8C%EC%82%AC%EC%A0%84
-# DFS
-# word_list에 A, AA, AAA, AAAA, AAAAA을 넣으면서 DFS를 돌다가
-# cnt = 5일 때 되돌아가면 AAAAE, AAAAI, AAAAO, AAAAU를 넣다가
-# AAAE, AAAEA, AAAEI ... 를 넣게 된다. 이런 식으로 완전 탐색 진행
-# 우리가 찾는 word의 순서는 word_list에서 word의 인덱스 + 1
+# 참고 : https://moondol-ai.tistory.com/423
+# A=1, E=?, I=1563 이렇게 보면 A와 I 간의 차이는 1562이기 때문에 A와 E 간의 차이는 아마 그 절반인 781이 아닐까 추측
+# 각 자릿수가 바뀌는 규칙은 (x 5) + 1 이다. 이를 각 자릿수에 적용하면 단어 수가 총 5개이므로 4개까지 반복하면 된다(781에 해당).
+# 코드에서 answer = len(word) 로 초기화한 이유는 딕셔너리 char에서 A를 0으로 두었기 때문이다. 하지만 AAAAA=5이므로 길이로 초기값을 정한 것이다.
+
 
 
 def solution(word):
-    answer = 0
-    word_list = []
-    words = 'AEIOU'
-    
-    def dfs(cnt, w):
-        if cnt == 5:
-            return 
-        for i in range(len(words)):
-            word_list.append(w + words[i])
-            dfs(cnt+1, w + words[i])
-            
-    dfs(0,"")
-    
-    return word_list.index(word)+1
+    char = {'A': 0, 'E': 1, 'I': 2, 'O': 3, 'U': 4}
+    answer = len(word) # A를 0으로 두었기 때문에 길이로 초기화 필요
+    re = (((5 + 1) * 5 + 1) * 5 + 1) * 5 + 1 # 781
+    for i in word:
+        answer += re * char[i] # 첫 문자가 무슨 글자로 시작하는지
+        re = (re - 1) // 5
+    return answer
