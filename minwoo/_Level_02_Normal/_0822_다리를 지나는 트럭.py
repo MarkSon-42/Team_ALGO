b632de5f16b26d7f80b7ee5847d8b69bd87f1fb0
minwoo <mark42son@gmail.com>
2023-08-22 18:33:17 +0900

    comment : added variables answer initiailizing

--- minwoo/_Level_02_Normal/_0822_다리를 지나는 트럭.py
+++ minwoo/_Level_02_Normal/_0822_다리를 지나는 트럭.py
@@ -2,7 +2,8 @@ from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
 
-    answer = 1
+    answer = 1  # 문제 테케 1번 표를 보면 0~1초 사이에는 아무것도 처리도 X ->
+    # gpt :  불연속 시간 간격을 처리할 때 1초 또는 문제 컨텍스트에 적합한 다른 임의의 시작점부터 시간 계산을 시작하는 것이 일반적
     queue = deque()
 
     total = truck_weights.pop(0)
@@ -22,3 +23,5 @@ def solution(bridge_length, weight, truck_weights):
             queue.append(0)
 
     return answer
+
+# 훨씬 빠르다.
