88136d2e0469c3bca1bcf53fb005e99ab41e3e86
minwoo <mark42son@gmail.com>
2023-08-25 20:00:38 +0900

    added addition solution _ import functools

---
+++ minwoo/_Level_02_Normal/_0825_가장 큰 수(functools).pyi
@@ -0,0 +1,13 @@
+# 아직 이해 못한 코드.
+import functools
+
+def comparator(a,b):
+    t1 = a+b
+    t2 = b+a
+    return (int(t1) > int(t2)) - (int(t1) < int(t2)) #  t1이 크다면 1  // t2가 크다면 -1  //  같으면 0
+
+def solution(numbers):
+    n = [str(x) for x in numbers]
+    n = sorted(n, key=functools.cmp_to_key(comparator),reverse=True)
+    answer = str(int(''.join(n)))
+    return answer
