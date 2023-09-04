532f6fb7a1a51cc82fae7f2fe634a5e5aa60a0f4
Hyungjoon Cho <fhrek@naver.com>
2023-09-04 18:17:04 +0900

    복습_백준_2644_촌수계산 - BFS 풀이 추가

--- hyungjoon/백준/DFS_BFS/230904_실버II_2644_촌수계산/solved(풀이참조).py
+++ hyungjoon/백준/DFS_BFS/230904_실버II_2644_촌수계산/solved(풀이참조).py
@@ -1,29 +1,37 @@
-# n = int(input())
-# a, b = map(int, input().split())
-# m = int(input())
+from collections import deque
+n = int(input())
+a, b = map(int, input().split())
+m = int(input())
 
-# graph = [ [] for _ in range(n+1)]
-# visited = [False] * (n+1)
+graph = [ [] for _ in range(n+1)]
+visited = [False] * (n+1)
 
-# for _ in range(m):
-#     # x = 부모, y = 자식
-#     x, y = map(int, input().split())
-#     graph[x].append(y)
-#     graph[y].append(x)
+for _ in range(m):
+    # x = 부모, y = 자식
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
 
-# answer = 0
-# cnt = 0
-# # 예제에서, 3->1, 1->2, 2->7 해서 3촌이다. 이 cnt 조건을 어떻게 할건가?
-# # a에서 시작해서 b를 만날때까지 계속 cnt를 쌓는다.
-# def dfs(v):
-#     visited[v] = True
-#     for i in graph[v]:
-#         if not visited[i]:
-#             global answer, cnt, b
-#             cnt += 1
-#             if i == b:  
-#                 answer = cnt
-#             dfs(i)
+arr = [0] * (n+1)
+# 예제에서, 3->1, 1->2, 2->7 해서 3촌이다. 이 cnt 조건을 어떻게 할건가?
+# a에서 시작해서 b를 만날때까지 계속 cnt를 쌓는다.
+def dfs(v):
+    visited[v] = True
+    for i in graph[v]:
+        if not visited[i]:
+            arr[i] = arr[v] + 1
+            dfs(i)
 
-# dfs(a)
-# print(-1 if answer == 0 else answer)
+def bfs(v):
+    q = deque([v])
+    visited[v] = True
+    while q:
+        t = q.popleft()
+        for i in graph[t]:
+            if not visited[i]:
+                arr[i] = arr[t] + 1
+                visited[i] = True
+                q.append(i)
+
+bfs(a)
+print(-1 if arr[b] == 0 else arr[b])
