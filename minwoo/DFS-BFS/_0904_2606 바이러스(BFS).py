9c5f2c66fc14eb9371d6bc86c8f80f751681ac33
minwoo <mark42son@gmail.com>
2023-09-04 19:34:49 +0900

    boj 2606 bfs solution

--- minwoo/DFS-BFS/_0904_2606 바이러스(BFS).py
+++ minwoo/DFS-BFS/_0904_2606 바이러스(BFS).py
@@ -1,54 +1,28 @@
-# from collections import deque
-#
-# N, M, V = map(int, input().split())
-#
-
-#
-# for _ in range(M):
-#     a, b = map(int, input().split())
-#     graph[a][b] = True  # 양방향을 초기화
-#     graph[b][a] = True
-#
-# visited_dfs = [False] * (N + 1)  # dfs 방문기록 초기화
-# visited_bfs = [False] * (N + 1)  # bfs 방문기록 초기화
-#
-# def dfs(V):
-#     visited_dfs[V] = True  # V는 시작노드. 시작 노드를 방문처리
-#     print(V, end = " ")
-#     for i in range(1, N + 1):
-#         if not visited_dfs[i] and graph[V][i]:
-#             dfs(i)
-#
-#
-#
-# def bfs(V):
-#     queue = deque([V])
-#     visited_bfs[V] = True
-#     while queue:  # queue가 빌 때 까지
-#         V.queue.popleft()
-#         print(V, end=" ")
-#         for i in range(1, N + 1):
-#             if not visited_bfs[i] and graph[V][i]:
-#                 queue.append(i)
-#                 visited_bfs[i] = True
-#
-#
-
-# 신종 웜 바이러스
 from collections import deque
-
-visited_bfs = [[F]]
-def bfs(V):
-
-
-
 N = int(input())
 M = int(input())
 
-graph = [[False] * (N + 1) for _ in range(N + 1)]
+#  True False대신 1, 0 으로 초기화 하는 방법
 
+pc = [[0] * (N + 1) for _ in range(N + 1)]
 for _ in range(M):
     a, b = map(int, input().split())
-    graph[a][b] = True
-    graph[b][a] = True
+    pc[a][b] = 1
+    pc[b][a] = 1
+visited = [0] * (N + 1)
 
+q = deque()
+q.append(1)
+
+visited[1] = 1
+cnt = 0
+while q:
+    current = q.popleft()
+    if pc[current][i] == 1 and visited[i] == 0:
+        #  "연결되어 있고 방문한 적 없으면"
+        visited[i] = 1
+        q.append(i)
+        cnt += 1
+
+
+print(cnt)
