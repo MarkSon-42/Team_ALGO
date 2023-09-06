4b63146225aa17ba8c4c7c4fd0880d0af50ad59e
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-09-06 18:50:18 +0900

    9/6

--- jongwon/백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ jongwon/백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -4,11 +4,11 @@ sys.setrecursionlimit(5000)
 input = sys.stdin.readline
 
 
-def dfs(v,depth):
+def dfs(v):
     visited[v] = True
     for i in graph[v]:
         if not visited[i]:
-            dfs(i,depth+1)
+            dfs(i)
 
 n,m = map(int, input().split())
 
@@ -22,15 +22,15 @@ for _ in range(m):
 visited= [False] * (n+1)
 cnt = 0
 
-# 1~N번 노드를 각각돌면서
+
 for i in range(1, n + 1):
-    if not visited[i]:  # 만약 i번째 노드를 방문하지 않았다면
-        if not graph[i]:  # 만약 해당 정점이 연결된 그래프가 없다면
-            cnt += 1  # 개수를 + 1
-            visited[i] = True  # 방문 처리
-        else:  # 연결된 그래프가 있다면
-            dfs(i, 0)  # dfs탐색을 돈다.
-            cnt += 1  # 개수를 +1
+    if not visited[i]:  
+        if not graph[i]:  
+            cnt += 1  
+            visited[i] = True  
+        else:  
+            dfs(i)  
+            cnt += 1 
 
 print(cnt)
     
