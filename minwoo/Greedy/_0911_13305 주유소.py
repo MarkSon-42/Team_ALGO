2a4c2008a411f1c90b1d2d9e47016e13896b3022
minwoo <mark42son@gmail.com>
2023-09-11 15:17:53 +0900

    error : indexError

---
+++ minwoo/Greedy/_0911_13305 주유소.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+
+N = int(input())
+
+roadway = list(map(int, input().split()))
+
+cost = list(map(int, input().split()))
+
+# 그리디 .. 첫 번째 값 설정
+
+min_val = roadway[0] * cost[0]
+
+# 가장 저렴한 주유소
+
+min_cost = cost[0]
+
+for i in range(1, N):
+    if min_cost > cost[i]:
+        min_cost = cost[i]
+    min_val += min_cost * roadway[i]
+
+print(min_val)
