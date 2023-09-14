d8bb8a748fcfd352cc878453f779d0e05daa9d3d
minwoo <mark42son@gmail.com>
2023-09-14 19:59:16 +0900

    0914 greedy 2212

--- minwoo/Simulation/플레이페어 암호.py
+++ minwoo/Simulation/플레이페어 암호.py
@@ -1,11 +1,1 @@
-import sys
-input = sys.stdin.readline
-
-n, m = map(int, input().split())
-num_list = list(map(int, input().split()))
-
-for _ in range(m):
-    num_list.sort()
-    num_list[0] = num_list[1] = num_list[0] + num_list[1]
-
-print(sum(num_list))
+# softeer 기출문제 lv3
