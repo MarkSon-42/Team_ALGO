eb2da7d047f3724501f186b708b3408d3326d84a
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-10-11 19:58:25 +0900

    10/11

--- jongwon/백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ jongwon/백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -1,14 +1,20 @@
-import heapq as hp
+import heapq
 import sys
 
-input = sys.stdin.readline
-pq = []
-for _ in range(int(input())):
-    x = int(input())
-    if x:
-        hp.heappush(pq,(abs(x), x))
+arr = []
+
+heapq.heapify(arr) # arr을 heapq로 변경
+
+n = int(sys.stdin.readline())
+
+for _ in range(n):
+    x = int(sys.stdin.readline())  
+    # x가 0이 아니면 heappush
+    if x != 0: 
+        heapq.heappush(arr,(abs(x),x)) # 우선 순위, 값형식의 튜플로 저장
+    # x가 0이면 heappop
     else:
-        if pq:
-            print(hp.heappop(pq)[1])
+        if arr:
+            print(heapq.heappop(arr)[1])
         else:
             print(0)
