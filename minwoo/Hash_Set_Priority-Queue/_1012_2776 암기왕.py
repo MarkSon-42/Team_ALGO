ac75c3866a350e5ee03959a7387eea21eebd8c65
minwoo <mark42son@gmail.com>
2023-10-12 19:56:23 +0900

    1012_7662 pq failed (2nd quiz)

--- minwoo/Hash_Set_Priority-Queue/_1012_2776 암기왕.py
+++ minwoo/Hash_Set_Priority-Queue/_1012_2776 암기왕.py
@@ -1,15 +1,14 @@
 # set 문제인듯
 # 그냥 list탐색하면 시초날게 분명 n, m ~1_000_000
 
-
 t = int(input())
 for _ in range(t):
     n = int(input())
-    note_1st = set(input().split())
+    note_1st = set(map(int, input().split()))
     m = int(input())
-    note_2nd = set(input().split())
+    note_2nd = map(int, input().split())
     for number in note_2nd:
-        if number in note_2nd:
+        if number in note_1st:
             print(1)
         else:
             print(0)
