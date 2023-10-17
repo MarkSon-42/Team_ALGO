38f11cab1d5f315f886dc25b9f1dd80503c0de2a
JongWon Kim <127610150+BellOne4222@users.noreply.github.com>
2023-10-17 18:53:08 +0900

    10/17

--- jongwon/백준/Gold/1918. 후위 표기식/후위 표기식.py
+++ jongwon/백준/Gold/1918. 후위 표기식/후위 표기식.py
@@ -4,32 +4,37 @@
 
 import sys
 
+# 중위 표기법 수식을 입력으로 받음
 s = sys.stdin.readline().rstrip()
+
+# 결과를 저장할 문자열
 result = ''
+
+# 연산자를 저장할 스택
 stack = []
 
-for i in s:  # 중위표기식의 각 원소에 대해
-    if i.isalpha():  # 알파벳이면 result에 곧바로 추가
+for i in s:  # 중위 표기식의 각 원소에 대해
+    if i.isalpha():  # 알파벳(피연산자)이면 result에 곧바로 추가
         result += i
     else:
-        if i == '(':
+        if i == '(':  # 여는 괄호를 만나면 스택에 추가
             stack.append(i)
-        elif i in ('+', '-'):  # 가장 우선순위가 낮은 연산자
-            while stack and stack[-1] != '(':  # 여는 괄호를 만나면 멈추고
-                result += stack.pop()  # 그렇지 않으면 stack 원소를 result에 추가
+        elif i in ('+', '-'):  # 가장 낮은 우선순위의 연산자
+            while stack and stack[-1] != '(':  # 여는 괄호를 만날 때까지
+                result += stack.pop()  # 스택의 연산자를 result에 추가
             stack.append(i)
-        elif i in ('/', '*'):
-            # 동일 우선순위 연산자는 먼저 나오는 연산자가 우선
+        elif i in ('/', '*'):  # 동일 우선순위의 연산자는 먼저 나온 연산자가 우선
             while stack and stack[-1] in ('/', '*'):
                 result += stack.pop()
             stack.append(i)
         elif i == ')':  # 닫는 괄호를 만나면
-            while stack and stack[-1] != '(':  # 괄호 내 모든 연산자를 result에 추가
-                result += stack.pop()
+            while stack and stack[-1] != '(':  # 여는 괄호를 만날 때까지
+                result += stack.pop()  # 스택의 연산자를 result에 추가
             stack.pop()
 
-# 알고리즘이 끝나고 스택에 원소가 있으면 모두 result에 추가
+# 알고리즘이 끝나고 스택에 남아있는 연산자들을 모두 result에 추가
 while stack:
     result += stack.pop()
 
+# 후위 표기법으로 변환된 수식 출력
 print(result)
