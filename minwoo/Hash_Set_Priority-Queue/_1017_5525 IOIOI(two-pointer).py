30b028d5753af16d3ce46b5384cda979f8d542bf
minwoo <mark42son@gmail.com>
2023-10-17 20:00:12 +0900

    1918 -ing

---
+++ minwoo/Hash_Set_Priority-Queue/_1017_5525 IOIOI(two-pointer).py
@@ -0,0 +1,24 @@
+# 이해 아직 못함.
+# 투포인터 풀이인데 복잡도는 비슷해 보이는데..????
+# 동일하게 문자열 순회
+# 뒤에 굳이 불필요한 탐색을 안하긴 할듯.
+
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+M = int(input())
+S = input().rstrip()
+left, right = 0, 0
+answer = 0
+
+while right < M:
+    if S[right:right + 3] == 'IOI':
+        right += 2
+        if right - left == 2 * N:
+            answer += 1
+            left += 2
+    else:
+        left = right = right + 1
+
+print(answer)
---
+++ minwoo/Hash_Set_Priority-Queue/_1017_5525 IOIOI.py
@@ -0,0 +1,25 @@
+
+# 'IOI'를 찾으면 된다.
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+s = input()
+
+answer, cnt, i = 0, 0, 0
+
+while i <= (m - 2):
+    if s[i:i + 3] == 'IOI':  # IOI 발견시
+        i += 2  # IO건너뛰고 찾아야하니 인덱스 조정
+        cnt += 1  # IOI패턴의 개수
+        if cnt == n:  # IOI패턴의 개수가 곧 원하는 Pn   ,  n = 2 -> 'IOIOI'
+            answer += 1
+            cnt -= 1  # 다시 cnt 감소시켜서 IOI 검색
+
+    else:
+        i += 1
+        cnt = 0
+
+print(answer)
