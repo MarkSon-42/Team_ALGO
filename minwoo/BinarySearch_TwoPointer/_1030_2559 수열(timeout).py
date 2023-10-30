2aba7162fe37e11b00d9b58beb0c9402ee837df6
minwoo <mark42son@gmail.com>
2023-10-30 17:09:35 +0900

    2559 timeout

---
+++ minwoo/BinarySearch_TwoPointer/_1030_2559 수열(timeout).py
@@ -0,0 +1,27 @@
+# 부분합을 반복문 돌려야 하나 싶었는데
+# 그냥 sum()에 슬라이싱 하면 그만임..!
+# 근데, 테케는 다 맞는데 시간초과..  O(N)이라서? 아니면 무한루프?
+# n : 2 ~ 100,000
+# k : 1 ~ n
+# elem :  -100 ~ 100
+import sys
+
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
+days = list(map(int, input().split()))
+
+left = 0
+right = k - 1
+max_sum = 0
+# print(sum(days[left:right + 1]))
+
+while right < n - 1:
+    partial_sum = sum(days[left:right + 1])
+    if max_sum <= partial_sum:
+        max_sum = partial_sum
+    left += 1
+    right += 1
+
+print(max_sum)
