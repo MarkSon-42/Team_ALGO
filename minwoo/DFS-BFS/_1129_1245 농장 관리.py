9ce96e2baf426e3314fb68efcfd5b2d361528674
minwoo <mark42son@gmail.com>
2023-11-29 22:53:55 +0900

    1245 bfs

--- minwoo/DFS-BFS/_1129_1245 농장 관리.py
+++ minwoo/DFS-BFS/_1129_1245 농장 관리.py
@@ -19,16 +19,17 @@ def bfs(row, col, idx):
     while queue:
         x, y = queue.popleft()
         for i in range(8):
-            nx, ny = row + dx[i], col + dy[i]
-            if nx < 0:
-                pass
-            if nx < 0:
-                pass
-            if nx < 0:
-                pass
-            if nx < 0:
-                pass
-
+            nx, ny = x + dx[i], y + dy[i]
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            if visited[nx][ny] == 1:
+                continue
+            if grid[x][y] < grid[nx][ny]:  # 봉우리 조건에 안맞을 때 (360도 방향 다 작아야함)
+                return 0
+            if grid[x][y] == grid[nx][ny]:
+                visited[nx][ny] = 1
+                queue.append((nx, ny))
+                check.append((nx, ny))
         idx += check
 
     return 1
@@ -37,7 +38,7 @@ def bfs(row, col, idx):
 mountain = 0
 
 for i in range(n):
-    for j in range(n):
+    for j in range(m):
         if (i, j) not in idx:
             visited = [[0] * m for _ in range(n)]
             mountain += bfs(i, j, idx)
