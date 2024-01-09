ce6f224c6de6844592b98f1032bb25a59172611e
minwoo <mark42son@gmail.com>
2024-01-09 17:36:31 +0900

    0109 2nd 공원산책

--- minwoo/_Level_01_Hard/덧칠하기.py
+++ minwoo/_Level_01_Hard/덧칠하기.py
@@ -10,3 +10,14 @@ def solution(n, m, section):
 
 
 solution(8, 4, [2, 3, 6])
+
+
+def solution_2(n, m, section):
+    answer = 0
+    while len(section) > 0:
+        temp = section[0] + m
+        while len(section) != 0 and temp > section[0]:
+            section.pop(0)
+        answer += 1
+
+    return answer
