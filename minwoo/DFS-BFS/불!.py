e34fbc55e5464d441621a763c8c77ee62b9dc9f7
minwoo <mark42son@gmail.com>
2024-03-06 18:00:14 +0900

    0306 boj 4179 improved code

--- minwoo/DFS-BFS/불!.py
+++ minwoo/DFS-BFS/불!.py
@@ -2,45 +2,40 @@ import sys
 from collections import deque
 
 input = sys.stdin.readline
-
 n, m = map(int, input().split())
+
 graph = []
-
-for i in range(n):
-    graph.append(list(input().rstrip()))
-    if 'J' in graph[i]:
-        queue = deque([(0, i, graph[i].index('J'))])
-
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 'F':
-            queue.append((-1, i, j))
-
+q = deque()
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-answer = 'IMPOSSIBLE'
+visited = [[0] * m for _ in range(n)]
 
-while queue:
-    time, x, y = queue.popleft()
+for i in range(n):
+    graph.append(list(input()))
+    for j in range(m):
+        if graph[i][j] == 'F':  # 불난곳은 큐 앞으로
+            visited[i][j] = 1
+            q.appendleft((i, j, 'F'))
+        elif graph[i][j] == 'J':  # 지훈이는 큐 뒤로
+            visited[i][j] = 1
+            q.append((i, j, 'J'))
 
-    # 지훈이 탈출
-    if time > -1 and graph[x][y] != 'F' and (x == 0 or y == 0 or x == n - 1 or y == m - 1):
-        answer = time + 1
-        break
+def bfs(q):
+    while q:
+        x, y, s = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if (0 <= nx < n) and (0 <= ny < m):
+                if visited[nx][ny] == 0 and graph[nx][ny] == '.':
+                    visited[nx][ny] = visited[x][y] + 1
+                    q.append((nx, ny, s))
+            else:
+                if s == 'J':
+                    return visited[x][y]
 
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] != '#':
-
-            # 지훈이 이동
-            if time > -1 and graph[nx][ny] == '.':
-                graph[nx][ny] = '_'
-                queue.append((time + 1, nx, ny))
-
-            # 불 퍼뜨리기
-            elif time == -1 and graph[nx][ny] != 'F':
-                graph[nx][ny] = 'F'
-                queue.append((-1, nx, ny))
-
-print(answer)
+ans = bfs(q)
+if ans:
+    print(ans)
+else:
+    print('IMPOSSIBLE')
