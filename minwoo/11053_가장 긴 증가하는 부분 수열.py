2354add81e59da81d9d8ecebb7bd02a8e918532d
minwoo <mark42son@gmail.com>
2024-05-01 16:22:39 +0900

    0501 lis and topological

--- minwoo/11053_가장 긴 증가하는 부분 수열.py
+++ minwoo/11053_가장 긴 증가하는 부분 수열.py
@@ -1,2 +1,30 @@
+# https://www.youtube.com/watch?v=SEdX0CrOfCg
+
+# 강의참고
+
+# LIS, LCS.... 수열 시리즈
+
+#
+
 n = int(input())
-arr = []
+arr = [0] + list(map(int, input().split())) + [0]  # 양 옆에 0 깔아줘야 루프시 비교가능.. ㄷㄷ
+
+lis = [0 for i in range(n + 2)]
+lisr = [0 for i in range(n + 2)]
+
+for i in range(1, n+1):
+    for j in range(0, i):
+        if arr[j] < arr[i]:
+            lis[i] = max(lis[i], lis[j] + 1)
+
+for i in range(n, 0, -1):
+    for j in range(n+1, i, -1):
+        if arr[i] > arr[j]:
+            lisr[i] = max(lisr[i], lisr[j] + 1)
+
+max_bitonic = 0
+
+for i in range(1, n+1):
+    max_bitonic = max(max_bitonic, lis[i] + lisr[i])
+
+print(max_bitonic -1)
