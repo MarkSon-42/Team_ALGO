5461a96b2c1748508c0a0ea17316f9a5ff103354
minwoo <mark42son@gmail.com>
2024-05-12 13:11:25 +0900

    refactor io file

--- minwoo/main.py
+++ minwoo/main.py
@@ -1,53 +1,33 @@
-# 삼성 식 빡구현 문제인데.. 
+#  1. 전체 배열 순회 ( 시작점 start i, start j )
 
-# 말의 이동 : 1번부터 k번말 순서대로 이동함. 1번 움직이고 2번 움직이고..
-
-# 1. 이동하고자 하는 칸이 흰색인 경우 : 그대로 감. 뒤집지 않음.
-
-
-# 2. 이동하고자 하는 칸이 빨간색인 경우  : 말 쌓인걸 반대로 뒤집는다. 다른말이 있으면 그 위에 뒤집은채로 올린다.
-
-
-# 3. 이동하고자 하는 칸이 파란색인 경우  : 이동 방향을 반대로 바꾸고  좌 -> 우  , 상 -> 하 한칸 이동. 방향 반대로 하고 가는 칸도 파란색이면 이동하지 않음.
-
-#  게임이 종료되는 턴의 번호를 출력한다. 그 값이 1,000보다 크거나 절대로 게임이 종료되지 않는 경우에는 -1을 출력
-
-# K 번째 말 :
-#  h[i][0] -> 행 번호
-#  h[i][1] -> 열 번호
-#  h[i][2] -> 이동 방향
-# k번까지 이동 찍을때마다 카운트를 늘려준다.
-from collections import deque
 import sys
-sys.stdin = open('input.txt', 'r')  # 6번 예제를 보면서 디버깅하면서 구현 진행
 
+sys.stdin = open('input.txt', 'r')
 
-N, K = map(int, input().split())
-matrix = [list(map(int, input().split())) for i in range(1, N + 1)]
-horse_info = [list(map(int, input().split())) for i in range(1, K + 1)]
-horse = deque()  # 말 스택.. 이 아니라 큐로 해야 하는거 아닌가?
-# [A,B,C] [D,E] -> DEABC
-#
-# dx = [0, 0, 1, -1]
-# dy = [1, -1, 0, 0]
-# 1~k번말을 알파멧으로 매겨줘야함?
+def dfs(n, ci, cj, v):
+    global answer
+    if n > 3:
+        return
+    if n == 3 and (si, sj) == (ci, cj):
+        answer = max(answer, len(v))
+        return
 
+    for k in range(n, n + 2):
+        ni, nj = ci + di[k], cj + dj[k]
+        if 0 <= ni < N and 0 <= nj < N and arr[ni][nj] not in v:
+            v.append(arr[ni][nj])
+            dfs(k, ni, nj, v)
+            v.pop()
 
-# 1 -> right
-# 2 -> left
-# 3 -> up
-# 4 -> down
+di = [1, 1, -1, -1, 1]
+dj = [-1, 1, 1, -1, -1]
+T = int(input())
+for tc in range(1, T + 1):
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    answer = -1
+    for si in range(N - 2):
+        for sj in range(1, N - 1):
+            dfs(0, si, sj, [])
 
-answer = 0
-
-# 말들을 겹치게 올리는건 어떻게 해야 하는..? stack?
-
-while True:
-    for i in range(1, K + 1):  # 턴을 계속 반복하면서 루프 끝나면 카운트
-        horse.appendleft()
-        horse_pos = [horse_info[i][0], horse_info[i][1]]
-        dr = horse_info[i][2]
-
-        # ...
-
-        if matrix[]
+    print(f"#{tc} {answer}")
